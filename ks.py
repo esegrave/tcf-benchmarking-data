40ee1a32f00c6ee4fae0c379cb1e030e34403dff
Eric Segraves <Eric.Segraves@nike.com>
2020-06-12 15:48:10 -0700

    remove old py

--- ks.py
+++
@@ -1,9 +0,0 @@
-# imports
-
-# district name/ school id mapper. Enrollment is missing school ID, Proficiency is missing district name
-
-# process enrollment
-
-# process proficiency
-
-# export dataframes
